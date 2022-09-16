f0119160d9669cedcba4c8789f2f49cde55a3ab0
최한나 <hanna0926@coway.com>
2022-09-16 17:54:48 +0900

    user/repo/commit 검색 TabBarLayout 임시커밋

--- lib/presentation/screens/search/search_screen.dart
+++ lib/presentation/screens/search/search_screen.dart
@@ -12,6 +12,11 @@ class _SearchScreenState extends State<SearchScreen>
     with TickerProviderStateMixin {
   late TabController _tabController;
 
+  final TextEditingController _uUserNameController = TextEditingController();
+  final TextEditingController _rUserNameController = TextEditingController();
+  final TextEditingController _cUserNameController = TextEditingController();
+  final TextEditingController _cRepoNameController = TextEditingController();
+
   final Color _selectedColor = CwColors.color1;
   final Color _unselectedColor = CwColors.dark_gray;
 
@@ -29,6 +34,7 @@ class _SearchScreenState extends State<SearchScreen>
     return Scaffold(
       body: Column(
         children: [
+          //TabBar
           Padding(
             padding: const EdgeInsets.fromLTRB(10, 20, 10, 10),
             child: Container(
@@ -55,36 +61,161 @@ class _SearchScreenState extends State<SearchScreen>
               ),
             ),
           ),
-          _tabScreens(_selectedIndex),
+
+          //검색조건 화면
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: [
+              searchUserScreen(),
+              searchRepoScreen(),
+              searchCommitScreen()
+            ].elementAt(_selectedIndex),
+          ),
+
+          const SizedBox(
+            height: 10,
+          ),
+
+          //검색 버튼
+          GestureDetector(
+            onTap: () {
+              if (_selectedIndex == 0) {
+              } else if (_selectedIndex == 1) {
+              } else if (_selectedIndex == 2) {}
+            },
+            child: Container(
+              width: 80,
+              height: 40,
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10.0),
+                  ),
+                  color: CwColors.color2),
+              child: const Center(
+                child: Text('검색'),
+              ),
+            ),
+          ),
+
+          const SizedBox(
+            height: 10,
+          ),
+
+          //검색결과화면
+          Expanded(
+            child: Container(
+              color: Colors.grey,
+              child: Center(child: Text('검색 결과화면')),
+            ),
+          )
         ],
       ),
     );
   }
 
+  Widget searchUserScreen() {
+    return Row(
+      children: [
+        const Text('user name : '),
+        SizedBox(
+          width: 250,
+          height: 50,
+          child: TextFormField(
+            controller: _uUserNameController,
+            keyboardType: TextInputType.text,
+            decoration: const InputDecoration(
+              hintText: 'User',
+              hintStyle: TextStyle(
+                fontSize: 20,
+                color: CwColors.dark_gray,
+              ),
+              filled: true,
+              fillColor: CwColors.gray,
+              border: InputBorder.none,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   Widget searchCommitScreen() {
-    return Container(
-      child: Text('search user screen'),
+    return Column(
+      children: [
+        Row(
+          children: [
+            const Text('user name : '),
+            SizedBox(
+              width: 250,
+              height: 50,
+              child: TextFormField(
+                controller: _cUserNameController,
+                keyboardType: TextInputType.text,
+                decoration: const InputDecoration(
+                  hintText: 'User',
+                  hintStyle: TextStyle(
+                    fontSize: 20,
+                    color: CwColors.dark_gray,
+                  ),
+                  filled: true,
+                  fillColor: CwColors.gray,
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            const Text('repo name : '),
+            SizedBox(
+              width: 250,
+              height: 50,
+              child: TextFormField(
+                controller: _cRepoNameController,
+                keyboardType: TextInputType.text,
+                decoration: const InputDecoration(
+                  hintText: 'Repo',
+                  hintStyle: TextStyle(
+                    fontSize: 20,
+                    color: CwColors.dark_gray,
+                  ),
+                  filled: true,
+                  fillColor: CwColors.gray,
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+          ],
+        )
+      ],
     );
   }
 
   Widget searchRepoScreen() {
-    return Container(
-      child: Text('search repo screen'),
+    return Row(
+      children: [
+        const Text('user name : '),
+        SizedBox(
+          width: 250,
+          height: 50,
+          child: TextFormField(
+            controller: _rUserNameController,
+            keyboardType: TextInputType.text,
+            decoration: const InputDecoration(
+              hintText: 'User',
+              hintStyle: TextStyle(
+                fontSize: 20,
+                color: CwColors.dark_gray,
+              ),
+              filled: true,
+              fillColor: CwColors.gray,
+              border: InputBorder.none,
+            ),
+          ),
+        ),
+      ],
     );
-  }
-
-  Widget searchUserScreen() {
-    return Container(
-      child: Text('search commit screen'),
-    );
-  }
-
-  Widget _tabScreens(int index) {
-    var tabScreens = <Widget>[];
-    tabScreens.add(searchCommitScreen());
-    tabScreens.add(searchRepoScreen());
-    tabScreens.add(searchUserScreen());
-    return tabScreens[index];
   }
 
   Widget tabButton(String buttonTxt, int buttonIndex) {
